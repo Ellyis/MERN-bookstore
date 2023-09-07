c2e3319c0ae11d46eaa18dc8934b3243b489c25b
Kai Bin <kaibinyong2002@gmail.com>
2023-09-07 14:32:04 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
-// import mongoose from 'mongoose';
-const mongoose = require('mongoose');
-// import cors from 'cors';
-const cors = require('cors');
+import mongoose from 'mongoose';
+import cors from 'cors';
 import { MONGODBURL, PORT } from './config.js';
 import { Book } from './models/bookModel.js';
 import bookRoute from './routes/bookRoute.js';
