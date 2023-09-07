9cfe4eb83c03c090af1ec76865edfc81a7a75475
Kai Bin <kaibinyong2002@gmail.com>
2023-09-07 12:25:34 +0800

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { MONGODBURL, PORT } from './config.js';
+import { Book } from './models/bookModel.js';
+import bookRoute from './routes/bookRoute.js';
+
+const app = express();
+
+// MIddleware for parsing request body
+app.use(express.json());
+
+// MIddleware for handling CORS policy
+app.use(cors());
+
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }));
+
+app.use('/books', bookRoute);
+
+mongoose.connect(MONGODBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
